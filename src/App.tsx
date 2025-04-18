7b85bf5f2ae547542c26bc6be95019cba13220a3
escapist-berlin <denis.kolokolov@icloud.com>
2025-04-18 21:08:27 +0200

    feat(FUL-1): add submit button to App component and use parseDiscogsInput from inputParser

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { parseDiscogsInput } from "./utils/inputParser";
 
 function App() {
   const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    const releaseId = parseDiscogsInput(input);
+    console.log('releaseId', releaseId);
+  };
 
   return (
     <>
@@ -11,17 +20,25 @@ function App() {
           <p className="text-lg text-gray-600 italic">
             from partial to full
           </p>
-          <p className="text-sm text-gray-500 mb-6">
+          <p className="text-sm text-gray-500 mb-4">
             Paste a Discogs release link and we’ll find full YouTube matches for every track
           </p>
 
           <input
             type='text'
             placeholder='Paste Discogs release URL...'
-            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full px-4 py-2 border border-gray-300 mb-4 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
+
+          <button
+            onClick={handleSubmit}
+            className="bg-purple-400 text-white px-4 py-2 rounded hover:bg-purple-500"
+            disabled={loading}
+          >
+            {loading ? 'Loading...' : 'Fetch Release'}
+          </button>
         </div>
       </div>
     </>
